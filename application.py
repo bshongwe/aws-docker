8847b015f9f8e41f4a87718622e389d9e7b1a392
Ernest Bhekizwe Shongwe <126109055+bshongwe@users.noreply.github.com>
2024-12-27 09:39:49 +0200

    Update application.py
    Externalised ENVs, graceful shutdown, reusable respnse gen, custom error handling >>> 405 && 404 --> HTML, debugging & modular structure

--- application.py
+++ application.py
@@ -1,168 +1,116 @@
 import logging
 import logging.handlers
-
+import os
 from wsgiref.simple_server import make_server, WSGIServer
 from socketserver import ThreadingMixIn
 
-# Create logger
+# Configuration
+LOG_FILE = os.getenv('LOG_FILE', '/tmp/sample-app/sample-app.log')
+PORT = int(os.getenv('PORT', 8000))
+MAX_LOG_SIZE = int(os.getenv('MAX_LOG_SIZE', 1048576))
+
+# Logging Configuration
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
-# Handler 
-LOG_FILE = '/tmp/sample-app/sample-app.log'
-handler = logging.handlers.RotatingFileHandler(LOG_FILE, maxBytes=1048576, backupCount=5)
-handler.setLevel(logging.INFO)
+file_handler = logging.handlers.RotatingFileHandler(LOG_FILE, maxBytes=MAX_LOG_SIZE, backupCount=5)
+console_handler = logging.StreamHandler()
 
-# Formatter
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(formatter)
+console_handler.setFormatter(formatter)
 
-# Add Formatter to Handler
-handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+logger.addHandler(console_handler)
 
-# add Handler to Logger
-logger.addHandler(handler)
-
-welcome = """
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+# HTML Templates
+WELCOME_HTML = """
+<!DOCTYPE html>
 <html>
 <head>
-  <!--
-    Copyright 2012 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-
-    Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
-
-        http://aws.Amazon/apache2.0/
-
-    or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
-  -->
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta charset="UTF-8">
   <title>Welcome</title>
-  <style>
-  body {
-    color: #ffffff;
-    background-color: #E0E0E0;
-    font-family: Arial, sans-serif;
-    font-size:14px;
-    -moz-transition-property: text-shadow;
-    -moz-transition-duration: 4s;
-    -webkit-transition-property: text-shadow;
-    -webkit-transition-duration: 4s;
-    text-shadow: none;
-  }
-  body.blurry {
-    -moz-transition-property: text-shadow;
-    -moz-transition-duration: 4s;
-    -webkit-transition-property: text-shadow;
-    -webkit-transition-duration: 4s;
-    text-shadow: #fff 0px 0px 25px;
-  }
-  a {
-    color: #0188cc;
-  }
-  .textColumn, .linksColumn {
-    padding: 2em;
-  }
-  .textColumn {
-    position: absolute;
-    top: 0px;
-    right: 50%;
-    bottom: 0px;
-    left: 0px;
-
-    text-align: right;
-    padding-top: 11em;
-    background-color: #24B8EB;
-  }
-  .textColumn p {
-    width: 75%;
-    float:right;
-  }
-  .linksColumn {
-    position: absolute;
-    top:0px;
-    right: 0px;
-    bottom: 0px;
-    left: 50%;
-    background-color: #A9A9A9;
-  }
-
-  h1 {
-    font-size: 500%;
-    font-weight: normal;
-    margin-bottom: 0em;
-  }
-  h2 {
-    font-size: 200%;
-    font-weight: normal;
-    margin-bottom: 0em;
-  }
-  ul {
-    padding-left: 1em;
-    margin: 0px;
-  }
-  li {
-    margin: 1em 0em;
-  }
-  </style>
 </head>
-<body id="sample">
-  <div class="textColumn">
-    <h1>Congratulations!</h1>
-    <p>Your Docker Container is now running in Elastic Beanstalk on your own dedicated environment in the AWS Cloud.</p>
-    <p>This environment is launched with Elastic Beanstalk Docker Platform</p>
-  </div>
-  
-  <div class="linksColumn"> 
-    <h2>Video Tutorials</h2>
-    <ul>
-    <li>YouTube: <a href="https://www.youtube.com/watch?v=lBu7Ov3Rt-M&feature=youtu.be">Run a Docker Container from the Docker Registry</a></li>
-    <li>YouTube: <a href="https://www.youtube.com/watch?v=pLw6MLqwmew&feature=youtu.be">Use Private Docker Repositories</a></li>
-    </ul>
-    <h2>Sample Apps</h2>
-    <ul>
-    <li>GitHub: <a href="https://github.com/awslabs/eb-demo-php-simple-app/tree/docker-apache">PHP and Amazon RDS</a></li>
-    <li>GitHub: <a href="https://github.com/awslabs/eb-py-flask-signup/tree/docker">Python, DynamoDB, and SNS</a></li>
-    </ul>
-    <h2>Documentation</h2>
-    <ul>
-    <li><a href="http://docs.aws.amazon.com/elasticbeanstalk/latest/dg/create_deploy_docker.html">Deploying Docker with AWS Elastic Beanstalk</a></li>
-    <li><a href="http://docs.aws.amazon.com/elasticbeanstalk/latest/dg/">AWS Elastic Beanstalk overview</a></li>
-    <li><a href="http://docs.aws.amazon.com/elasticbeanstalk/latest/dg/index.html?concepts.html">AWS Elastic Beanstalk concepts</a></li>
-    </ul>
-  </div>
+<body>
+  <h1>Congratulations!</h1>
+  <p>Your Docker Container is running in Elastic Beanstalk on AWS.</p>
 </body>
 </html>
 """
 
+ERROR_404_HTML = """
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>404 Not Found</title>
+</head>
+<body>
+  <h1>404 Not Found</h1>
+  <p>The requested resource was not found on this server.</p>
+</body>
+</html>
+"""
+
+# Response Helpers
+def generate_response(status, body, content_type='text/html'):
+    """Generate an HTTP response."""
+    headers = [('Content-Type', content_type)]
+    return status, headers, body.encode()
+
+# WSGI Application
 def application(environ, start_response):
-    path    = environ['PATH_INFO']
-    method  = environ['REQUEST_METHOD']
-    if method == 'POST':
-        try:
+    try:
+        path = environ.get('PATH_INFO', '/')
+        method = environ.get('REQUEST_METHOD', 'GET')
+        client_ip = environ.get('REMOTE_ADDR', 'Unknown')
+
+        logger.info("Received %s request for %s from %s", method, path, client_ip)
+
+        if method == 'POST':
             if path == '/':
-                request_body_size = int(environ['CONTENT_LENGTH'])
-                request_body = environ['wsgi.input'].read(request_body_size)
-                logger.info("Received a message.")
+                try:
+                    request_body_size = int(environ.get('CONTENT_LENGTH', 0))
+                    request_body = environ['wsgi.input'].read(request_body_size).decode('utf-8')
+                    logger.info("POST to / with body: %s", request_body)
+                except (TypeError, ValueError) as e:
+                    logger.warning("Error reading POST body: %s", str(e))
+                return generate_response('200 OK', "")
             elif path == '/scheduled':
-                logger.info("Received task %s scheduled at %s", environ['HTTP_X_AWS_SQSD_TASKNAME'], environ['HTTP_X_AWS_SQSD_SCHEDULED_AT'])
-        except (TypeError, ValueError):
-            logger.warning('Error retrieving request body for async work.')
-        response = ''
-    else:
-        response = welcome
-    status = '200 OK'
-    headers = [('Content-type', 'text/html')]
+                task_name = environ.get('HTTP_X_AWS_SQSD_TASKNAME', 'Unknown')
+                scheduled_at = environ.get('HTTP_X_AWS_SQSD_SCHEDULED_AT', 'Unknown')
+                logger.info("Scheduled task: %s at %s", task_name, scheduled_at)
+                return generate_response('200 OK', "")
+            else:
+                logger.warning("Unhandled POST path: %s", path)
+                return generate_response('404 Not Found', ERROR_404_HTML)
+        elif method == 'GET':
+            if path == '/':
+                return generate_response('200 OK', WELCOME_HTML)
+            else:
+                logger.warning("Unhandled GET path: %s", path)
+                return generate_response('404 Not Found', ERROR_404_HTML)
+        else:
+            logger.warning("Unhandled HTTP method: %s", method)
+            return generate_response('405 Method Not Allowed', "<h1>405 Method Not Allowed</h1>")
 
-    start_response(status, headers)
-    return [response.encode()]
+    except Exception as e:
+        logger.error("Unhandled exception: %s", str(e))
+        return generate_response('500 Internal Server Error', "<h1>500 Internal Server Error</h1>")
 
-class ThreadingWSGIServer(ThreadingMixIn, WSGIServer): 
+    finally:
+        logger.info("Request processing completed.")
+
+# Threaded Server
+class ThreadingWSGIServer(ThreadingMixIn, WSGIServer):
     pass
 
 if __name__ == '__main__':
-    httpd = make_server('', 8000, application, ThreadingWSGIServer)
-    print("Serving on port 8000...")
+    httpd = make_server('', PORT, application, ThreadingWSGIServer)
+    logger.info(f"Serving on port {PORT}...")
     try:
         httpd.serve_forever()
-    except KeyboardInterrupt:
-        pass
+    except (KeyboardInterrupt, SystemExit):
+        logger.info("Shutting down server gracefully.")
+    except Exception as e:
+        logger.error("Error while running server: %s", str(e))
